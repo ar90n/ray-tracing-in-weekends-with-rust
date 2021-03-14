622e6aad8aa3a9405f0925aff70b62ecc959511c
Masahiro Wada <argon.argon.argon@gmail.com>
2021-03-14 12:03:41 +0000

    feat: add diffuse support

--- first_weekend/src/main.rs
+++ first_weekend/src/main.rs
@@ -14,14 +14,20 @@ use ray::Ray;
 use sphere::Sphere;
 use vec3::{unit_vector, Vec3};
 
-fn color(r: &Ray, world: &dyn Hitable) -> Vec3 {
+fn color(r: &Ray, world: &dyn Hitable, rng: &mut rand::rngs::ThreadRng) -> Vec3 {
     if let Some(hit_record) = world.hit(r, 0.0, 1000.0) {
-        return 0.5
-            * Vec3::new(
-                hit_record.normal.x() + 1.0,
-                hit_record.normal.y() + 1.0,
-                hit_record.normal.z() + 1.0,
-            );
+        let random_in_unit_sphere = {
+            let r = rng.gen::<f32>();
+            let theta = 2.0 * 3.14 * rng.gen::<f32>();
+            let phi = 2.0 * 3.14 * rng.gen::<f32>();
+            Vec3::new(
+                r * phi.sin() * theta.cos(),
+                r * phi.sin() * theta.sin(),
+                r * phi.cos(),
+            )
+        };
+        let target = hit_record.p + hit_record.normal + random_in_unit_sphere;
+        return 0.5 * color(&Ray::new(hit_record.p, target - hit_record.p), world, rng);
     }
 
     let unit_direction = unit_vector(r.direction());
@@ -54,9 +60,10 @@ fn main() {
                     let u = (i as f32 + rng.gen::<f32>()) / nx as f32;
                     let v = (j as f32 + rng.gen::<f32>()) / ny as f32;
                     let r = cam.get_ray(u, v);
-                    col += color(&r, &world);
+                    col += color(&r, &world, &mut rng);
                 }
-                col / (ns as f32)
+                col /= ns as f32;
+                Vec3::new(col.x().sqrt(), col.y().sqrt(), col.z().sqrt())
             };
 
             let ir = (255.99 * col[0]) as i32;
